a99822a388c910cfce6730dc7884121dae4f3c9f
MaorShmueli55 <maorshmueli5@gmail.com>
2024-12-18 10:27:22 +0200

    1

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Teachers from "./components/ui/teachers";
+import { AppSidebar } from "./components/SideBar";
+import { SidebarTrigger } from "./components/ui/sidebar";
 
 
 function App() {
   return (
     <Router>
+          <AppSidebar />
+          <SidebarTrigger />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/teachers" element={<Teachers />} />
       </Routes>
     </Router>
